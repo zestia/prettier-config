39c689c1c85d118f7ffabd80d4bf05e74ef3c1d4
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-07-16 16:23:32 +0100

    rely on defaults

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSpacing: true,
   printWidth: 100,
-  quoteProps: 'as-needed',
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'none'
+  singleQuote: true
 };
